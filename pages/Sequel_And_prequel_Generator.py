8b00b6cee3541a5b16fd24e80d99fd78e067fb08
Nitiz Khanal <67851367+StarAtNyte@users.noreply.github.com>
2023-03-03 21:33:26 +0545

    Update Sequel_And_prequel_Generator.py

--- pages/Sequel_And_prequel_Generator.py
+++ pages/Sequel_And_prequel_Generator.py
@@ -1,6 +1,6 @@
-from setup import chatbot
 from pdf import PDF
-import youtube_dl
+#import youtube_dl
+import yt_dlp as youtube_dl
 import requests
 import streamlit as st
 import textwrap
@@ -15,25 +15,17 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import re
-from PIL import Image 
-import PIL 
-import urllib.request
-import cv2
-import torch
-import torchaudio
-import torch.nn as nn
-import torch.nn.functional as F
+import openai
 
-import IPython
-import pydub
-from pydub import AudioSegment
-from pydub.playback import play
-import moviepy.editor as mp
+ 
+openai.api_key = "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"
+# Environment Variable for Replicate
+os.environ["REPLICATE_API_TOKEN"] = "b3ea4715f5e3450de2093c2c82fd224208a069e3"
 
-from tortoise.api import TextToSpeech
-from tortoise.utils.audio import load_audio, load_voice, load_voices
-
+stability_api = client.StabilityInference(
+    key='sk-FMO2lOKk4jwqehIGpDxfnxFt5ctfkKWcEtaZCXMxiKC1UmKT',
+    verbose=True,
+)
 
 #function for getting transcription of audio from youtube video
 def get_transcript(link):
@@ -92,22 +84,13 @@ def get_transcript(link):
 
 
 
-# Environment Variable for Replicate
-os.environ["REPLICATE_API_TOKEN"] = "b3ea4715f5e3450de2093c2c82fd224208a069e3"
-
-stability_api = client.StabilityInference(
-    key='sk-D32IvqLxlRBfl40mix3QVTfEcWkANId4slFtRkuB1VTJOujb', 
-    verbose=True,
-)
 # PDF Object
 pdf = PDF()
 cover_pdf = PDF()
 foreword_pdf = PDF()
 summary_pdf = PDF()
 
-import replicate
-midj_model = replicate.models.get("tstramer/midjourney-diffusion")
-midj_version = midj_model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+
 
 st.title('Get Sequel/Prequel of your favourite Story')
 st.text('''
@@ -137,8 +120,15 @@ preq_seq = st.selectbox(
 
 generate_title = st.button('Generate the title')
 if generate_title:
-    response = chatbot.ask( f'Generate only 5 words title for {preq_seq} of the following story: {st.session_state.story}')
-    st.session_state.title = response['message']
+    response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt= f'Generate only 5 words title for {preq_seq} of the following story: {st.session_state.story}',
+                max_tokens = 5,
+                temperature=0.6
+            )
+    response = response['choices'][0]['text']
+    #response = chatbot.ask( f'Generate only 5 words title for {preq_seq} of the following story: {st.session_state.story}')
+    st.session_state.title = response
     st.text(st.session_state.title)
 
 author = "BookAI"
@@ -194,19 +184,33 @@ complete_text =''
 ## PDF Body
 completed = False
 if st.button('Get PDF'):
-    st.write('Writing Your Storyy.')
-    st.markdown("![Writing Your Story](https://media.giphy.com/media/YAnpMSHcurJVS/giphy.gif)")
+    st.write('Good things take time :), Processing...')
 
     text = []
-    response = chatbot.ask( f"Generate {chapters} chapter titles for the story {st.session_state.title}")
-    chaps= response['message'].rsplit("\n")
+    response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=f"Generate {chapters} chapter titles for the story {st.session_state.title}",
+                max_tokens = 100,
+                temperature=0.6
+            )
+
+    chaps = response['choices'][0]['text'].rsplit('\n')
+    #response = chatbot.ask( f"Generate {chapters} chapter titles for the story {st.session_state.title}")
+    #chaps= response['message'].rsplit("\n")
     
 
     for i in range(1,chapters+1):
-        response = chatbot.ask( f"generate content for Chapter {i-1}: {chaps[i-1]}")
-        if response['message'][0:2] == "In":
-            response = chatbot.ask( f"generate content for chapter {i}")
-        text.append(response['message'])
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"generate content for chapter {i} :{chaps[i-1][4:-1]} of the novel titled {st.session_state.title}",
+            max_tokens = 300,
+            temperature=0.6
+        )
+        text.append(response['choices'][0]['text'])
+        #response = chatbot.ask( f"generate content for Chapter {i-1}: {chaps[i-1]}")
+        #if response['message'][0:2] == "In":
+            #response = chatbot.ask( f"generate content for chapter {i}")
+        #text.append(response['message'])
         complete_text += text[0]
 
     # Text to TXT
@@ -263,8 +267,13 @@ if st.button('Get PDF'):
     #summary_pdf.output(f'about_{title}.pdf', 'F')
     
     #Foreword generation
-    foreword_response = chatbot.ask( f"write foreword for the book written by you on the title {st.session_state.title}, in the style of an experienced writer, 400 words")
-    foreword = "FOREWORD\n\n\n\n" + foreword_response['message']
+    foreword_response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=f"write foreword for the book written by you on the title {st.session_state.title}, in the style of an experienced writer, 400 words",
+                max_tokens = 300,
+                temperature=0.6
+            )
+    foreword = "FOREWORD\n\n\n\n" + foreword_response['choices'][0]['text']
     with open (f'foreword.txt', 'w') as file:  
             file.write(foreword)
     foreword_pdf.print_chapter(i, f"Foreword", f'foreword.txt')
@@ -293,87 +302,14 @@ if completed:
     )
 
 
-# if st.button('Get Audio Book'):
 
-
-#     file = open("chapter0.txt", "r")
-#     doclist = [ line for line in file ]
-#     docstr = '' . join(doclist)
-#     sentences = re.split(r'[.!?]', docstr)
-
-#     for i in range(len(sentences)):
-#         output = midj_version.predict(prompt=sentences[i])
-#         urllib.request.urlretrieve(output[0], f"images/image{i}.jpg")
-
-
-#     def generate_video():
-#         image_folder = 'images' # Use the folder
-#         video_name = 'mygeneratedvideo.avi'
-#         os.chdir("videos")
-#         images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") or
-#                 img.endswith(".jpeg") or img.endswith("png")]
-    
-#         fourcc = cv2.VideoWriter_fourcc(*'DIVX') 
-
-#         # Array images should only consider 
-#         # the image files ignoring others if any 
-        
-#         frame = cv2.imread(os.path.join(image_folder, images[0]))
-        
-#         # setting the frame width, height width 
-#         # the width, height of first image 
-#         height, width, layers = frame.shape
-
-#         video = cv2.VideoWriter(video_name, fourcc, 0.15, (width, height))
-
-#         # Appending the images to the video one by one 
-#         for image in images:
-#             video.write(cv2.imread(os.path.join(image_folder,image)))
-            
-#         # Deallocating memories taken for window creation 
-#         cv2.destroyAllWindows()
-#         video.release()  # releasing the video generated 
-        
-#     generate_video()
-
-
-#     tts = TextToSpeech()
-
-#     text = sentences
-
-#     # Pick a "preset mode" to determine quality. Options: {"ultra_fast", "fast" (default), "standard", "high_quality"}. See docs in api.py
-#     preset = "fast"
-
-#     voice = 'train_dotrice'
-
-#     # Load it and send it through Tortoise.
-#     voice_samples, conditioning_latents = load_voice(voice)
-#     for i in range(len(sentences)):
-#         gen = tts.tts_with_preset(sentences[i], voice_samples=voice_samples, conditioning_latents=conditioning_latents, preset=preset)
-#         torchaudio.save(f'generated{i}.wav', gen.squeeze(0).cpu(), 24000)
-
-
-
-#     sound1 = AudioSegment.from_wav("tortoise-tts/generated0.wav")
-
-#     for i in range(0,len(sentences)-1):
-#         sound2 = AudioSegment.from_wav(f"tortoise-tts/generated{i+1}.wav")
-#         combined = sound1 + sound2
-#         sound1 = combined
-
-#     combined.export("combined.wav", format="wav")
-
-
-#     sound = pydub.AudioSegment.from_wav("tortoise-tts/combined.wav")
-#     sound.export("tortoise-tts/combined.mp3", format="mp3")
-
-
-#     clip = mp.VideoFileClip('videos/mygeneratedvideo.avi')
-#     clip.write_videofile('videos/mygeneratedvideo.mp4')
-
-
-#     audio = mp.AudioFileClip('tortoise-tts/combined.mp3')
-#     video = mp.VideoFileClip('videos/mygeneratedvideo.mp4')
-#     final_video = video.set_audio(audio)
-
-#     final_video.write_videofile("output_video.mp4")
+##if st.button('Get Audio Book'):
+##    # pdf to audio
+##    audio_model = replicate.models.get("afiaka87/tortoise-tts")
+##    audio_version = audio_model.versions.get("e9658de4b325863c4fcdc12d94bb7c9b54cbfe351b7ca1b36860008172b91c71")
+##    reader = PdfReader("dummy.pdf")
+##    text = ""
+##    for page in reader.pages:
+##        text += page.extract_text() + "\n" 
+##    output = audio_version.predict(text=text)
+##    st.audio(output, format='audio/ogg')
