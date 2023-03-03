afcc1f0a3a8993b0f19c4dacf36d2c9dc92b1015
Pratik Shrestha <47277011+P-stha12@users.noreply.github.com>
2023-03-03 22:02:23 +0545

    bed time story

--- pages/Bedtime_Story_Generator.py
+++ pages/Bedtime_Story_Generator.py
@@ -1,4 +1,5 @@
-from setup import chatbot
+ 
+#from setup import chatbot
 from pdf import PDF
 import requests
 import streamlit as st
@@ -14,8 +15,8 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import openai
 
- 
-openai.api_key = "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"
+openai.api_key = "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"
+
 # Environment Variable for Replicate
 os.environ["REPLICATE_API_TOKEN"] = "b3ea4715f5e3450de2093c2c82fd224208a069e3"
 
@@ -39,7 +40,6 @@ Please follow the following instructions:
 3. Download button will appear after everything is completed.
 4. We recommend 3 chapters for optimal result.
 5. If any error occurs, please click the button of the step in which the error occured.
-
                     Thank you.
 ''')
 
@@ -62,7 +62,7 @@ model_id = "stabilityai/stable-diffusion-2-1"
 cover_pdf.add_page()
 #Cover page image
 if st.button('Get Cover Image'):
-    
+
     answers = stability_api.generate(
         prompt= f"Minima book Illustration of a children bedtime story, ({st.session_state.title})",
         width=768, # Generation width, defaults to 512 if not included.
@@ -79,24 +79,21 @@ if st.button('Get Cover Image'):
                 img_name = str(artifact.seed)+ ".png"
                 img.save(img_name)
                 image = Image.open(img_name)
-                
+
                 # Custom font style and font size
                 W = 768
                 title_font = ImageFont.truetype('playfair/playfair-font.ttf', 35)
                 author_font = ImageFont.truetype('playfair/playfair-font.ttf', 20)
                 title_text = f"{st.session_state.title}"
-                words_in_title = title_text.split(' ')
                 if ':' in title_text:
                     temp = title_text.split(':')
-                    title_text = temp[0] + '\t\t\n' + temp[1]
-                if len(words_in_title) >4:
-                    title_text = words_in_title[0:4] + '\t\n' + words_in_title[4:]
+                    title_text = temp[0] + '\n' + temp[1]
                 image_editable = ImageDraw.Draw(image)
                 w, h = image_editable.textsize(st.session_state.title)
                 image_editable.text(((W-w)/3.7,25), title_text, (237, 230, 211), font=title_font)
                 image_editable.text((630,1050), author, (237, 230, 211), font=author_font,align='left')
                 image.save("cover.jpg")
-                
+
                 cover_pdf.image("cover.jpg",x=0, y=0, w= 210, h= 297)
 
                 cover_pdf.output('cover.pdf', 'F')
@@ -114,39 +111,40 @@ if st.button('Get PDF'):
 
     text = []
     response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt= f"Generate {paragraphs} paragraph titles for the children bedtime story {st.session_state.title} with moral {st.session_state.moral}",
-                    max_tokens = 100,
-                    temperature=0.6
-                )
-
+                model="text-davinci-003",
+                prompt=f"Generate {paragraphs} paragraph titles for the children bedtime story {st.session_state.title}\
+                            with moral {st.session_state.moral}",
+                max_tokens = 100,
+                temperature=0.6
+            )
+    #response = chatbot.ask(f"Generate 10 chapter titles for the novel {title}")
+    #chaps= response['message'].rsplit("\n")
     chaps = response['choices'][0]['text'].rsplit('\n')
     chaps = [chap for chap in chaps if chap != '']
-    
+    #response = chatbot.ask( f"Generate {paragraphs} paragraph titles for the children bedtime story {st.session_state.title}\
+        #with moral {st.session_state.moral}")
+    #chaps= response['message'].rsplit("\n")
+
 
     for i in range(1,paragraphs+1):
         response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt= f"generate a paragraph for paragraph {i-1}: {chaps[i-1]}",
-                    max_tokens = 100,
-                    temperature=0.6
-                )
-        if response['message'][0:2] == "In":
-            response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt= f"generate a paragraph for paragraph {i}",
-                    max_tokens = 100,
-                    temperature=0.6
-                )
-
-        text.append(response['message'])
+            model="text-davinci-003",
+            prompt=f"generate a paragraph for paragraph {i} :{chaps[i-1][4:-1]} of the children book story titled {st.session_state.title}",
+            max_tokens = 300,
+            temperature=0.6
+        )
+        text.append(response['choices'][0]['text'])
+        #response = chatbot.ask( f"generate a paragraph for paragraph {i-1}: {chaps[i-1]}")
+        #if response['message'][0:2] == "In":
+            #response = chatbot.ask( f"generate a paragraph for paragraph {i}")
+        #text.append(response['message'])
         complete_text += text[0]
 
     # Text to TXT
     for i in range(0, paragraphs):
-        with open (f'chapter{i+1}.txt', 'w') as file:  
-            file.write(text[i])  
-    
+        with open (f'chapter{i+1}.txt', 'w') as file:
+            file.write(text[i])
+
 
 
     pdf.set_title(st.session_state.title)
@@ -168,29 +166,29 @@ if st.button('Get PDF'):
                     img_name = str(artifact.seed)+ ".png"
                     img.save(img_name)
                     image = Image.open(img_name)
-                
+
                 # Custom font style and font size
-                
+
                 title_font = ImageFont.load_default()
                 title_text = f"{st.session_state.title}"
                 image_editable = ImageDraw.Draw(image)
                 image_editable.text((15,15), title_text, (237, 230, 211), font=title_font)
                 image.save(f"{i-1}.jpg")
-                
-        pdf.print_chapter(i, f"{chaps[i-1][4:-1]}", f'chapter{i}.txt')
+
+        pdf.print_chapter(i, f"{chaps[i-1]}", f'chapter{i}.txt')
         pdf.image(f"{i-1}.jpg",x= 10, w=190, h = 80)
     pdf.output('dummy.pdf', 'F')
-    
+
     #cohere text summarization
-    #response = co.generate( 
-    #model='xlarge', 
+    #response = co.generate(
+    #model='xlarge',
     #prompt = complete_text,
-    #max_tokens=250, 
+    #max_tokens=250,
     #temperature=0.9)
 
     #summary = response.generations[0].text
     #pdf of summary
-    #with open (f'about_{title}.txt', 'w') as file:  
+    #with open (f'about_{title}.txt', 'w') as file:
     #        file.write(f"About {title}\n\n{summary}")
     #summary_pdf.print_chapter(i, f"About_{title}", f'about_{title}.txt')
     #summary_pdf.output(f'about_{title}.pdf', 'F')
@@ -214,19 +212,6 @@ if completed:
         btn=st.download_button(
         label="⬇️ Download PDF",
         data=file,
-        file_name=f"{st.session_state.title}.pdf",
+        file_name="bedtimestory.pdf",
         mime="application/octet-stream"
     )
-
-
-
-##if st.button('Get Audio Book'):
-##    # pdf to audio
-##    audio_model = replicate.models.get("afiaka87/tortoise-tts")
-##    audio_version = audio_model.versions.get("e9658de4b325863c4fcdc12d94bb7c9b54cbfe351b7ca1b36860008172b91c71")
-##    reader = PdfReader("dummy.pdf")
-##    text = ""
-##    for page in reader.pages:
-##        text += page.extract_text() + "\n" 
-##    output = audio_version.predict(text=text)
-##    st.audio(output, format='audio/ogg')
