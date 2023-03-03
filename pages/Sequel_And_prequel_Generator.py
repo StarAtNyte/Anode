7baf9bb6d2913db1b5fbf3df70c1a7583ad63044
Nitiz Khanal <67851367+StarAtNyte@users.noreply.github.com>
2023-03-03 22:50:33 +0545

    Update Sequel_And_prequel_Generator.py

--- pages/Sequel_And_prequel_Generator.py
+++ pages/Sequel_And_prequel_Generator.py
@@ -16,7 +16,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 #function for getting transcription of audio from youtube video
 def get_transcript(link):
